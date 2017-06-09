366eed20a1867ef779e75ccdfdca8ebd68828093
Dougly <dgalante11@gmail.com>
2017-06-09 16:00:35 -0400

    Movie class added

---
+++ iTunesTop25Movies/Movie.swift
@@ -0,0 +1,32 @@
+//
+//  Movie.swift
+//  iTunesTop25Movies
+//
+//  Created by Douglas Galante on 6/9/17.
+//  Copyright © 2017 Dougly. All rights reserved.
+//
+
+import UIKit
+
+class Movie {
+    
+    let title: String
+    let releaseDate: String
+    let price: String
+    let posterURLString: String
+    var posterImage: UIImage?
+    
+    init(with json: [String : Any]) {
+        self.title = json["im:name"] as! String
+        self.price = json["im:price"] as! String
+
+        let dateDictionary = json["im:releaseDate"] as! [String : Any]
+        let dateStringDictionary = dateDictionary["attributes"] as! [String : String]
+        self.releaseDate = dateStringDictionary["label"]!
+        
+        let imageDictionary = json["im:image"] as! [[String : Any]]
+        let imageURL = imageDictionary[2]["label"] as! String
+        self.posterURLString = imageURL
+    }
+    
+}
