74c818d7ff279ed7f92341b3cf9a5fb6be9ec97f
Dougly <dgalante11@gmail.com>
2017-06-09 16:15:39 -0400

    added view controllers

---
+++ MovieListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListViewController.swift
+//  iTunesTop25Movies
+//
+//  Created by Douglas Galante on 6/9/17.
+//  Copyright © 2017 Dougly. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ iTunesTop25Movies/MovieDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailViewController.swift
+//  iTunesTop25Movies
+//
+//  Created by Douglas Galante on 6/9/17.
+//  Copyright © 2017 Dougly. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
